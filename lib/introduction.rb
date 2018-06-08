a92df664c86e86f6ac5b6c04c8a12a118170d39c
whynotski <laurawynosky@gmail.com>
2018-06-08 15:39:39 -0400

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+# greeting
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+introduction("Josh")
+
+#greet_language
+def introduction_with_language(name, language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+ introduction_with_language("Dan", "Ember.js")
